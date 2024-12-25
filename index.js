2d51fd948f4c1db50817a7fbd50e3c0159e4ae10
cosmicviscera <192325773+cosmicviscera@users.noreply.github.com>
2024-12-25 16:39:49 +1100

    Deduplicate JS for allergens page

--- index.js
+++ index.js
@@ -1,177 +1,170 @@
+const locations = ["Melbourne CBD", "Ivanhoe", "Northcote",
+    "Footscray", "Frankston", "Dandenong", "Reservoir", "Preston"];
 
-$( document ).ready(function() {
+const horizontalLabels = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
+const CHART_COLORS = {
+    "ten": "#31309c",
+    "twenty": "#25afe4",
+    "thirty": "#24bbdb",
+    "fifty": "#2be79d",
+    "seventy": "#ddd62a",
+    "eighty": "#fc9a28",
+    "ninety": "#d1260b",
+    "hundred": "#650005"
+};
 
-    const locations = ["Melbourne CBD", "Ivanhoe", "Northcote", 
-        "Footscray", "Frankston", "Dandenong", "Reservoir", "Preston"];
+let width, height, gradient;
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+  if (!gradient || width !== chartWidth || height !== chartHeight) {
+    // Create the gradient because this is either the first render
+    // or the size of the chart has changed
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
+    gradient.addColorStop(0, CHART_COLORS["ten"]);
+    gradient.addColorStop(0.2, CHART_COLORS["twenty"]);
+    gradient.addColorStop(0.3, CHART_COLORS["thirty"]);
+    gradient.addColorStop(0.5, CHART_COLORS["fifty"]);
+    gradient.addColorStop(0.7, CHART_COLORS["seventy"]);
+    gradient.addColorStop(0.8, CHART_COLORS["eighty"]);
+    gradient.addColorStop(0.9, CHART_COLORS["ninety"]);
+    gradient.addColorStop(1, CHART_COLORS["hundred"]);
+  }
 
-    const horizontalLabels = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"];
-    const CHART_COLORS = {
-        "ten": "#31309c", 
-        "twenty": "#25afe4", 
-        "thirty": "#24bbdb",
-        "fifty": "#2be79d",
-        "seventy": "#ddd62a",
-        "eighty": "#fc9a28",
-        "ninety": "#d1260b",
-        "hundred": "#650005"
-    };
+  return gradient;
+}
 
-    let width, height, gradient;
-    function getGradient(ctx, chartArea) {
-      const chartWidth = chartArea.right - chartArea.left;
-      const chartHeight = chartArea.bottom - chartArea.top;
-      if (!gradient || width !== chartWidth || height !== chartHeight) {
-        // Create the gradient because this is either the first render
-        // or the size of the chart has changed
-        width = chartWidth;
-        height = chartHeight;
-        gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top);
-        gradient.addColorStop(0, CHART_COLORS["ten"]);
-        gradient.addColorStop(0.2, CHART_COLORS["twenty"]);
-        gradient.addColorStop(0.3, CHART_COLORS["thirty"]);
-        gradient.addColorStop(0.5, CHART_COLORS["fifty"]);
-        gradient.addColorStop(0.7, CHART_COLORS["seventy"]);
-        gradient.addColorStop(0.8, CHART_COLORS["eighty"]);
-        gradient.addColorStop(0.9, CHART_COLORS["ninety"]);
-        gradient.addColorStop(1, CHART_COLORS["hundred"]);
-      }
-
-      return gradient;
+function generateBarColors(values) {
+    var barColors = [];
+    for (let i = 0; i < values.length; i++) {
+        if (values[i] < 10) {
+            barColors[i] = CHART_COLORS["ten"];
+        } else if (values[i] < 20) {
+             barColors[i] = CHART_COLORS["twenty"];
+        } else if (values[i] < 30) {
+            barColors[i] = CHART_COLORS["thirty"];
+        } else if (values[i] < 50) {
+            barColors[i] = CHART_COLORS["fifty"];
+        } else if (values[i] < 70) {
+            barColors[i] = CHART_COLORS["seventy"];
+        } else if (values[i] < 80) {
+            barColors[i] = CHART_COLORS["eighty"];
+        } else if (values[i] < 90) {
+            barColors[i] = CHART_COLORS["ninety"];
+        } else {
+            barColors[i] = CHART_COLORS["hundred"];
+        }
     }
 
-    function initialiseMultiForecast(dataPoints, canvasClass, parentId, isMultiForecast) {
-        for (let i = 0; i < dataPoints.length; i++) {
-            const canvasId = dataPoints[i] + "Chart";
-            var heightString = "";
-            var horizontalLabelDisplay = true;
-            var lastItem = i == dataPoints.length - 1;
-            var dataLabel = "";
+    return barColors;
+}
 
-            // Only restrict height if we're showing multiple linegraphs
-            if (isMultiForecast) {
-                var height = "70px";
+// TODO this is boilerplate config, in the future it will use real data specific to the air quality metric in question
+function generateRandomForecast() {
+    const numbers = [];
+    for (let i = 0; i < 28; i++) {
+        numbers.push(Math.random() * 100);
+    };
+    return numbers;
+}
 
-                // Only show the left label if there's multiple forecasts
-                dataLabel = dataPoints[i];
+function initialiseMultiForecast(dataPoints, canvasClass, parentId, isMultiForecast) {
+    for (let i = 0; i < dataPoints.length; i++) {
+        const canvasId = dataPoints[i] + "Chart";
+        var heightString = "";
+        var horizontalLabelDisplay = true;
+        var lastItem = i == dataPoints.length - 1;
+        var dataLabel = "";
 
-                // Only show labels for the last graph in the set
-                // last graph in set needs a biger height due to quirks
-                if (lastItem) {
-                    height = "90px";
-                } else {
-                    horizontalLabelDisplay = false;
-                }
+        // Only restrict height if we're showing multiple linegraphs
+        if (isMultiForecast) {
+            var height = "70px";
 
-                // only include a height specification if multiforecast
-                heightString = "style='height: " + height + "'";
+            // Only show the left label if there's multiple forecasts
+            dataLabel = dataPoints[i];
+
+            // Only show labels for the last graph in the set
+            // last graph in set needs a biger height due to quirks
+            if (lastItem) {
+                height = "90px";
+            } else {
+                horizontalLabelDisplay = false;
             }
 
+            // only include a height specification if multiforecast
+            heightString = "style='height: " + height + "'";
+        }
 
-            // create canvases for each datapoint
 
-            $(parentId).append(
-                "<div  class='" + canvasClass + "'" + heightString + "><h4>"
-                + dataLabel + "</h4><canvas id='"
-                + canvasId
-                + "'></canvas></div>"
-            );
+        // create canvases for each datapoint
 
-            const currentCanvas = $("#" + canvasId);
+        $(parentId).append(
+            "<div  class='" + canvasClass + "'" + heightString + "><h4>"
+            + dataLabel + "</h4><canvas id='"
+            + canvasId
+            + "'></canvas></div>"
+        );
 
-            // set up chart 
-            const lineConfig = {
-              type: 'line',
-              data: {
-                  labels: horizontalLabels,
-                  // uncomment this to see a version combined into a single graph
-                  // datasets: generateAqiDatasets()
-                  datasets: [{
-                    label: "ppm", data: generateRandomForecast(), 
-                    borderColor: function(context) {
-                            const chart = context.chart;
-                            const {ctx, chartArea} = chart;
+        const currentCanvas = $("#" + canvasId);
 
-                            if (!chartArea) {
-                              // This case happens on initial chart load
-                              return;
-                            }
-                            return getGradient(ctx, chartArea);
-                    },
-                  }]
+        // set up chart
+        const lineConfig = {
+          type: 'line',
+          data: {
+              labels: horizontalLabels,
+              // uncomment this to see a version combined into a single graph
+              // datasets: generateAqiDatasets()
+              datasets: [{
+                label: "ppm", data: generateRandomForecast(),
+                borderColor: function(context) {
+                        const chart = context.chart;
+                        const {ctx, chartArea} = chart;
+
+                        if (!chartArea) {
+                          // This case happens on initial chart load
+                          return;
+                        }
+                        return getGradient(ctx, chartArea);
                 },
-                options: {
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    plugins: {
-                        legend: {
-                            display: false // hide the annoying label
+              }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                plugins: {
+                    legend: {
+                        display: false // hide the annoying label
+                    }
+                },
+                scales: {
+                    x: {
+                        grid: {
+                            display: !isMultiForecast
+                        },
+                        ticks: {
+                            display: horizontalLabelDisplay
                         }
                     },
-                    scales: {
-                        x: {
-                            grid: {
-                                display: !isMultiForecast
-                            },
-                            ticks: {
-                                display: horizontalLabelDisplay
-                            }
+                    y: {
+                        grid: {
+                            display: !isMultiForecast
                         },
-                        y: {
-                            grid: {
-                                display: !isMultiForecast
-                            },
-                            ticks: {
-                                display: !isMultiForecast
-                            }
+                        ticks: {
+                            display: !isMultiForecast
                         }
                     }
                 }
-            };
-            new Chart(currentCanvas, lineConfig);
-        };
-
-    };
-
-    // Update forecasts - TODO placeholder for when we have real data to update
-    function updateForecasts(location) {
-        console.log("Current location has been changed to: " + location);
-    }
-
-    function generateBarColors(values) {
-        var barColors = [];
-        for (let i = 0; i < values.length; i++) {
-            if (values[i] < 10) {
-                barColors[i] = CHART_COLORS["ten"];
-            } else if (values[i] < 20) {
-                 barColors[i] = CHART_COLORS["twenty"];               
-            } else if (values[i] < 30) {
-                barColors[i] = CHART_COLORS["thirty"];
-            } else if (values[i] < 50) {
-                barColors[i] = CHART_COLORS["fifty"];
-            } else if (values[i] < 70) {
-                barColors[i] = CHART_COLORS["seventy"];
-            } else if (values[i] < 80) {
-                barColors[i] = CHART_COLORS["eighty"];
-            } else if (values[i] < 90) {
-                barColors[i] = CHART_COLORS["ninety"];
-            } else {
-                barColors[i] = CHART_COLORS["hundred"];               
             }
-        }
-
-        return barColors;
+        };
+        new Chart(currentCanvas, lineConfig);
     }
 
+}
 
-    // TODO this is boilerplate config, in the future it will use real data specific to the air quality metric in question
-    function generateRandomForecast() {
-        const numbers = [];
-        for (let i = 0; i < 28; i++) {
-            numbers.push(Math.random() * 100);
-        };
-        return numbers;
-    };
 
+$( document ).ready(function() {
 
     /////////////// location picker //////////////
 
@@ -189,7 +182,11 @@ $( document ).ready(function() {
         var options = {searchable: true};
         NiceSelect.bind(document.getElementById("locationList"), options);
     }
-
     initialiseLocationPicker();
+
+    // Update forecasts on location change - TODO placeholder for when we have real data to update
+    function updateForecasts(location) {
+        console.log("Current location has been changed to: " + location);
+    }
 
 });
